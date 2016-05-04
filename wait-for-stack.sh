b4dccb14f7e2540b9cbe684bb67cf78dda670d35
Theresa Adams <Theresa.M.Adams@nsmtp.kp.org>
2016-05-04 12:51:03 -0700

    working on getting build to work

--- wait-for-stack.sh
+++ wait-for-stack.sh
@@ -2,8 +2,6 @@
 
 set -exu
 
-export DOCKER_IP="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$(hostname)")"
-
 for i in {1..5}; do
     curl "http://$DOCKER_IP:8000" && curl "http://$DOCKER_IP:4444" && break; sleep 5
 done
