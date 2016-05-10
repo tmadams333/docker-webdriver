cc9f9e9f77d050075be26d1fba0633499c34542f
Theresa Adams <Theresa.M.Adams@nsmtp.kp.org>
2016-05-10 10:36:22 -0700

    modified hello.py

--- uwsgi/hello.py
+++ uwsgi/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hey there!</h1><script>alert("Hello World!")</script>'
+    return '<h1>Hey there!  From the KP CSG DevOps Team led by our superb leader Alex Kvyat</h1><script>alert("Hello World!")</script>'
 
 if __name__ == "__main__":
     app.run()
